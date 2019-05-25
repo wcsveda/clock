8add5d1e55602f9b8b94668e7ce6452b1f4bcef8
naaspati <naaspati@gmail.com>
2019-05-25 08:59:43 +0530

    fixed misplaced hour_mark(s)

--- js/index.js
+++ js/index.js
@@ -2,16 +2,15 @@ function firstByClass(cls) {
     return document.getElementsByClassName(cls)[0];
 }
 
-const rad_per_deg = 0.0174533;
 const radius = 22;
 
 function set(n) {
     const element = firstByClass('hour_mark_'+n);
-    const rads = rad_per_deg * (n * 30 - 90);
+    const rads = ((n * 30 - 90) * Math.PI)/180;
     const x = Math.cos(rads) * radius;
     const y = Math.sin(rads) * radius;
 
-    element.style.transform = `translateX(${x}vh) translateY(${y}vh)`
+    element.style.transform = `translateX(${x - 1}vh) translateY(${y - 2}vh)`
 }
 
 for (let n = 1; n < 13; n++) 
